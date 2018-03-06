33639ac16b119e96b75f215419d09e729f6c7683
unknown <utha@metamorf.co.id>
2018-03-06 18:01:22 +0700

    add service & repository

--- src/main/java/com/sharingsession/repository/TestRepository.java
+++ src/main/java/com/sharingsession/repository/TestRepository.java
@@ -1,5 +1,8 @@
 package com.sharingsession.repository;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TestRepository {
 
 }
--- src/main/java/com/sharingsession/service/TestService.java
+++ src/main/java/com/sharingsession/service/TestService.java
@@ -1,5 +1,8 @@
 package com.sharingsession.service;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class TestService {
 
 }
